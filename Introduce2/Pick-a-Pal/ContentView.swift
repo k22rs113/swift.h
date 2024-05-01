09ed9d03091eca73caefe8b2eab7e7c5d9c3c667
k22rs113 <k22rs113@st.kyusan-u.ac.jp>
2024-05-01 13:06:30 +0900

    update

---
+++ Introduce2/Pick-a-Pal/ContentView.swift
@@ -0,0 +1,97 @@
+//
+//  ContentView.swift
+//  Pick-a-Pal
+//
+//  Created by h.kento on 2024/04/28.
+//
+
+import SwiftUI
+
+struct ToDoList:Codable {
+var isCheck:Bool
+var Tasks:String
+}
+
+
+struct ContentView: View {
+    //@State private var names: [String] = ["Elisha", "Andre", "Jasmine", "Po-Chan"]
+    @State private var names: [ToDoList] = []
+    
+    
+    @State private var nameToAdd = ""
+    //@State private var pickedName = ""
+    @State private var shouldRemovePickedName = false
+    @State private var check: [Bool] = []
+    var body: some View {
+        VStack {
+            VStack(spacing: 8) {
+                Image(systemName: "person.3.sequence.fill")
+                    .foregroundStyle(.tint)
+                    .symbolRenderingMode(.hierarchical)
+                Text("To-Do-List")
+            }
+            
+            TextField("タスクを入力", text: $nameToAdd)
+                .autocorrectionDisabled()
+                .textFieldStyle(.roundedBorder)
+                .onSubmit {
+                    if !nameToAdd.isEmpty {
+                        names.append(ToDoList(isCheck: false, Tasks: nameToAdd))
+                        nameToAdd = ""
+                    }
+                }
+            
+//            Text(pickedName.isEmpty ? " " : pickedName)
+//                .font(.title2)
+//                .bold()
+//                .foregroundStyle(.tint)
+            
+            List {
+                ForEach(names.indices, id: \.self) { index in
+                   HStack{
+                       Image(systemName: names[index].isCheck ? "checkmark.circle.fill" : "circle")
+                       Button( action: {
+                           names[index].isCheck.toggle()
+                       },
+                   label: {
+                       Text(names[index].Tasks)
+                               .foregroundColor(.black)
+                       })
+                           
+                   }
+                }
+           }
+           .clipShape(RoundedRectangle(cornerRadius: 8))
+            
+            
+            
+            
+            //Toggle("Remove picked Name", isOn: $shouldRemovePickedName)
+            
+            //          Button {
+            //                if let randomName = names.randomElement(){
+            //                    pickedName = randomName
+            //
+            //                    if shouldRemovePickedName {
+            //                        names.removeAll { name in
+            //                            return (name == randomName)
+            //                        }
+            //                    }
+            //                } else {
+            //                    pickedName = ""
+            //                }
+            //            } label: {
+            //                Text("Pick Random Name")
+            //                    .padding(.vertical, 8)
+            //                    .padding(.horizontal, 16)
+            //            }
+            //            .buttonStyle(.borderedProminent)
+            //            .font(.title2)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
